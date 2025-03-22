8377d4b42ad63abc4eaba3dcecebc4b3daa1bed2
Stef van Esch <stef@marshmallow.dev>
2025-03-22 14:04:38 +0100

    (update) moved mysql dump flags to config for better custimization

--- src/Actions/Mysql/DumpDataAction.php
+++ src/Actions/Mysql/DumpDataAction.php
@@ -13,7 +13,8 @@ class DumpDataAction
         Config $config,
         DatabaseSyncCommand $command,
     ): void {
-        $dumpCommand = "mysqldump -u {$config->remote_database_username} -p{$config->remote_database_password} --skip-lock-tables --no-create-info --complete-insert --skip-triggers --replace --where='id IN (SELECT id FROM {$table} WHERE created_at >= \\\"{$config->date} 00:00:00\\\" OR updated_at >= \\\"{$config->date} 00:00:00\\\")' {$config->remote_database} {$table}";
+        $dump_flags = config('database-sync.mysql.dump_action_flags');
+        $dumpCommand = "mysqldump -u {$config->remote_database_username} -p{$config->remote_database_password} {$dump_flags} --where='id IN (SELECT id FROM {$table} WHERE created_at >= \\\"{$config->date} 00:00:00\\\" OR updated_at >= \\\"{$config->date} 00:00:00\\\")' {$config->remote_database} {$table}";
 
         /**
          * Run all dump commands and save to a new .sql file
